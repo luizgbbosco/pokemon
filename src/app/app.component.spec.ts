4cf3c937e7e027f6406353631a86729bb9bbf278
luizgbbosco <luizgbbosco@gmail.com>
2023-12-26 12:09:38 -0300

    feature: new structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,11 +2,9 @@ import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [AppComponent],
-    }).compileComponents();
-  });
+  beforeEach(() => TestBed.configureTestingModule({
+    declarations: [AppComponent]
+  }));
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'pokemon' title`, () => {
+  it(`should have as title 'pokemon-v4'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('pokemon');
+    expect(app.title).toEqual('pokemon-v4');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, pokemon');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('pokemon-v4 app is running!');
   });
 });
